e5955a1ccdd4b0e9047c1a21015f12e9e2cd6fad
Ejaz Ahmed <ejaz_ahmed@outlook.com>
2015-12-25 16:49:44 +0500

    Adding utilities in trait

--- multitenant/src/main/groovy/org/grails/plugin/multitenant/Multitenant.groovy
+++ multitenant/src/main/groovy/org/grails/plugin/multitenant/Multitenant.groovy
@@ -1,5 +1,25 @@
 package org.grails.plugin.multitenant
 
 trait Multitenant {
+    def tenantResolverService
     Long tenantId
+
+    def beforeInsert() {
+        if(tenantId == null){
+            tenantId = tenantResolverService.resolveTenant()
+        }
+    }
+
+    def beforeValidate() {
+        if(tenantId == null){
+            tenantId = tenantResolverService.resolveTenant()
+        }
+    }
+
+    def beforeUpdate() {
+        if(tenantId == null){
+            tenantId = tenantResolverService.resolveTenant()
+        }
+    }
+
 }
