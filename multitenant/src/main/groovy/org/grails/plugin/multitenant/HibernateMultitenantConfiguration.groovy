36690c7d7c23c8b7cdecdaa79aac94a5a832956a
Ejaz Ahmed <ejaz_ahmed@outlook.com>
2015-12-25 07:05:39 +0500

    Cleanup

--- multitenant/src/main/groovy/org/grails/plugin/multitenant/HibernateMultitenantConfiguration.groovy
+++ multitenant/src/main/groovy/org/grails/plugin/multitenant/HibernateMultitenantConfiguration.groovy
@@ -28,44 +28,15 @@ class HibernateMultitenantConfiguration extends GrailsAnnotationConfiguration {
         }
 
         super.secondPassCompile()
-//        for (domainClass in grailsApplication.domainClasses) {
-//            def filters = domainClass.getPropertyValue('hibernateFilters')
-//            if (filters instanceof Closure) {
-//                new HibernateFilterBuilder(this, domainClass)
-//            }
-//        }
 
         this.addFilterDefinition new FilterDefinition("tenant", "tenant_id = :tenant", [tenant: this.typeResolver.basic('long')])
         for (domainClass in grailsApplication.domainClasses) {
             if (domainClass.clazz in Multitenant) {
-//                log.debug "Enricht ${domainClass} with tenant filter"
                 def entity = this.getClassMapping(domainClass.fullName)
                 entity.addFilter("tenant", "tenant_id = :tenant", false, null, null)
-
-                // Add multitenant utility functions
-//                entity.metaClass.static.withTenantId = { Long tenantId, Closure closure ->
-//                    Session session = sessionFactory.currentSession
-//                    withTenantId(session, tenantId, closure)
-//                }
-//                entity.metaClass.static.withOutTenantId = { Closure closure ->
-////                    Session session = sessionFactory.currentSession
-//                    printf("Doing without tenantId")
-//                    closure.doCall()
-////                    withOutTenantId(session, closure)
-//                }
             }
         }
 
         configLocked = true
     }
-
-//    static withOutTenantId(Session session, Closure closure) {
-//        try {
-//            session.disableFilter("tenant")
-//            closure.doCall()
-//        }
-//        finally {
-//            session.enableFilter("tenant")
-//        }
-//    }
 }
--- multitenant/src/main/groovy/org/grails/plugin/multitenant/MultitenantGrailsPlugin.groovy
+++ multitenant/src/main/groovy/org/grails/plugin/multitenant/MultitenantGrailsPlugin.groovy
@@ -41,31 +41,8 @@ Tenants are resolved using spring security. It does not support URL based tenant
     // Online location of the plugin's browseable source code.
 //    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
 
-//    Closure doWithSpring() { {->
-//            // TODO Implement runtime spring config (optional)
-//        }
-//    }
-
     void doWithDynamicMethods() {
-        // TODO Implement registering dynamic methods to classes (optional)
+
     }
 
-//    void doWithApplicationContext() {
-//        // TODO Implement post initialization spring config (optional)
-//    }
-//
-//    void onChange(Map<String, Object> event) {
-//        // TODO Implement code that is executed when any artefact that this plugin is
-//        // watching is modified and reloaded. The event contains: event.source,
-//        // event.application, event.manager, event.ctx, and event.plugin.
-//    }
-//
-//    void onConfigChange(Map<String, Object> event) {
-//        // TODO Implement code that is executed when the project configuration changes.
-//        // The event is the same as for 'onChange'.
-//    }
-//
-//    void onShutdown(Map<String, Object> event) {
-//        // TODO Implement code that is executed when the application shuts down (optional)
-//    }
 }
